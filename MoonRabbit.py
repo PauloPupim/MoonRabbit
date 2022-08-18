12de90a06c3cf71d2bb076c13ff445ac811296d6
PauloPupim <pandreengineer@gmail.com>
2022-08-18 02:21:05 -0300

    Added function to calculate time in a non-modal window

--- MoonRabbit.py
+++ MoonRabbit.py
@@ -1,18 +1,20 @@
-import random
-from random import randint
-import string
+import re
 import sys
 import json
 
+from datetime import timedelta
+
 from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QDoubleSpinBox, QAbstractSpinBox, QFileDialog, QMessageBox
+    QApplication, QMainWindow, QDoubleSpinBox, QAbstractSpinBox, QFileDialog, QMessageBox, QSpinBox, QLineEdit, QWidget
 )
-from PyQt6 import QtCore, QtGui
+from PyQt6 import QtCore
 from PyQt6.QtCore import QEvent, QObject
+
 
 # Command for generate ui class/file
 # pyuic6.exe -o ui_MoonRabbitWindow.py .\ui_files\MoonRabbitWindow_.ui
 from ui_MoonRabbitWindow import Ui_MainWindow
+from ui_CalculateTimeWindow import Ui_CalculateTime
 
 # Command to create rc_img
 # pyside6-rcc.exe .\img\img_files.qrc -o rc_img.py
@@ -31,7 +33,57 @@ class MouseWheelWidgetAdjustmentGuard(QObject):
         return super().eventFilter(o, e)
 
 
+class CalculateTime(QWidget, Ui_CalculateTime):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.connectSignals()
+
+    def connectSignals(self):
+        self.cb_rate_time.currentTextChanged.connect(self.updateTime)
+        self.dsb_rate.valueChanged.connect(self.updateTime)
+        self.dsb_needed.valueChanged.connect(self.updateTime)
+
+    def updateTime(object, value):
+        rate = object.dsb_rate.value()
+        needed = object.dsb_needed.value()
+        time_rate = object.cb_rate_time.currentText()
+        match time_rate:
+            case 'Hours':
+                time = 1/60
+            case 'Minutes':
+                time = 1
+            case 'Seconds':
+                time = 60
+        if rate <= 0:
+            result = ''
+        else:
+            minutes = needed / rate / time
+            result = '' if rate <= 0 else str(timedelta(minutes=minutes)).replace(',', '')
+            result = re.sub('\..*', '', result)
+
+        object.lineedit_result.setText(result)
+
+
 class MoonRabbitWindow(QMainWindow, Ui_MainWindow):
+    # Level = Enum('LEVEL', 'U1 U2 U3 U4 E1 E2 E3 E4 L1 L2 L3 L4 S1 S2 S3 S4')
+
+    # U1 = 1
+    # U2 = 2
+    # U3 = 3
+    # U4 = 4
+    # E1 = 5
+    # E2 = 6
+    # E3 = 7
+    # E4 = 8
+    # L1 = 9
+    # L2 = 10
+    # L3 = 11
+    # L4 = 12
+    # S1 = 13
+    # S2 = 14
+    # S3 = 15
+    # S4 = 16
     current_save_file = None
     unsaved_flag = False
     def __init__(self, parent=None):
@@ -52,9 +104,8 @@ class MoonRabbitWindow(QMainWindow, Ui_MainWindow):
         else:
             event.accept()
 
-
     def preventAnnoyingSpinboxScrollBehaviour(self, control: QAbstractSpinBox) -> None:
-        control.setFocusPolicy(Qt.StrongFocus)
+        control.setFocusPolicy(QtCore.FocusPolicy.StrongFocus)
         control.installEventFilter(self.MouseWheelWidgetAdjustmentGuard(control))
 
     def set_unsaved_flag(self):
@@ -68,11 +119,50 @@ class MoonRabbitWindow(QMainWindow, Ui_MainWindow):
     def connectSignalsSlots(self):
         self.actionSave_File.triggered.connect(self.save_file)
         self.actionLoad_File.triggered.connect(self.load_file)
+        self.actionCalculate_Time.triggered.connect(self.show_calculate_window)
+
         for spinbox in self.findChildren(QDoubleSpinBox):
+            name = spinbox.objectName()
+            character = 'nia' if name[-2:] == '_2' else 'rab'
+            type = name[6:] if character == 'rab' else name[6:-2]
             spinbox.valueChanged.connect(self.calculaNeeded)
             spinbox.setFocusPolicy(QtCore.Qt.FocusPolicy.StrongFocus)
             spinbox.installEventFilter(MouseWheelWidgetAdjustmentGuard(spinbox))
             spinbox.valueChanged.connect(self.set_unsaved_flag)
+
+            if type == 'needed':
+                spinbox.valueChanged.connect(self.calculaTime)
+
+        for spinbox in self.findChildren(QSpinBox):
+            spinbox.valueChanged.connect(self.calculaTime)
+            spinbox.valueChanged.connect(self.set_unsaved_flag)
+
+    def show_calculate_window(self):
+        # But if you see it it will only be visible for a fraction of a second. What's happening?
+        # Inside this method, we are creating our window (widget) object, storing it in the variable w and showing it.
+        # However, once we leave the method we no longer have a reference to the w variable (it is a local variable) and
+        # so it will be cleaned up – and the window destroyed. To fix this we need to keep a reference to the window somewhere, for example on the self object.
+        self.calculate = CalculateTime()
+        self.calculate.show()
+
+    def calculaTime(object, value):
+        name = object.sender().objectName()
+        level = name[3:5]
+        character = 'nia' if name[-2:] == '_2' else 'rab'
+        type = name[6:] if character == 'rab' else name[6:-2]
+
+        if type == 'needed':
+            needed = value
+            rate = object.findChild(QSpinBox, 'sb_' + level + '_rate').value()
+        else:
+            needed = object.findChild(QDoubleSpinBox, 'sb_' + level + '_needed').value()
+            rate = value
+
+        mnt = '' if rate <= 0 else str(timedelta(minutes=needed/(rate/60))).replace(',', '')
+
+        mnt = re.sub('\..*' ,'',mnt)
+
+        object.findChild(QLineEdit, 'le_' + level + '_time').setText(mnt)
 
     def calculaNeeded(object, value):
         name = object.sender().objectName()
@@ -244,13 +334,31 @@ class MoonRabbitWindow(QMainWindow, Ui_MainWindow):
         rab_dict = {}
         nia_dict = {}
 
-        for spinbox in self.findChildren(QDoubleSpinBox):
+        for doublespinbox in self.findChildren(QDoubleSpinBox):
+            if doublespinbox.value() != 0:
+                # Character Nia has a '_2' suffix in his objects names
+                if doublespinbox.objectName()[-2:] == '_2':
+                    nia_dict[doublespinbox.objectName()[:-2]] = doublespinbox.value()
+                else:
+                    rab_dict[doublespinbox.objectName()] = doublespinbox.value()
+
+        for spinbox in self.findChildren(QSpinBox):
             if spinbox.value() != 0:
                 # Character Nia has a '_2' suffix in his objects names
                 if spinbox.objectName()[-2:] == '_2':
                     nia_dict[spinbox.objectName()[:-2]] = spinbox.value()
                 else:
                     rab_dict[spinbox.objectName()] = spinbox.value()
+
+        for lineedit in self.findChildren(QLineEdit):
+            if lineedit.objectName()=='qt_spinbox_lineedit':
+                continue
+            if lineedit.text() != '':
+                # Character Nia has a '_2' suffix in his objects names
+                if lineedit.objectName()[-2:] == '_2':
+                    nia_dict[lineedit.objectName()[:-2]] = lineedit.text()
+                else:
+                    rab_dict[lineedit.objectName()] = lineedit.text()
 
         moon_dict = {'rab': rab_dict, 'nia': nia_dict}
         json_file = json.dumps(moon_dict, indent=4, sort_keys=True)
@@ -266,6 +374,7 @@ class MoonRabbitWindow(QMainWindow, Ui_MainWindow):
 
         if load_file == '':
             return
+        self.blockSignals(True)
 
         with open(load_file, 'r') as f:
             moon_data = json.loads(f.read())
@@ -274,15 +383,19 @@ class MoonRabbitWindow(QMainWindow, Ui_MainWindow):
                 for key in moon_data[character].keys():
                     # Character Nia has a '_2' suffix in his objects names
                     box = key if character == 'rab' else key+'_2'
-                    self.findChild(QDoubleSpinBox, box).setValue(moon_data[character][key])
-
+                    type = key[6:] #if character == 'rab' else key[6:-2]
+                    if type == 'atual' or type == 'needed' or type == 'wanted':
+                        self.findChild(QDoubleSpinBox, box).setValue(moon_data[character][key])
+                    if type == 'rate':
+                        self.findChild(QSpinBox, box).setValue(moon_data[character][key])
+                    if type == 'time':
+                        self.findChild(QLineEdit, box).setText(moon_data[character][key])
         self.current_save_file = load_file
         self.set_saved_flag()
+        self.blockSignals(False)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    moonRabbitWindow= MoonRabbitWindow()
+    moonRabbitWindow = MoonRabbitWindow()
     moonRabbitWindow.show()
     sys.exit(app.exec())
